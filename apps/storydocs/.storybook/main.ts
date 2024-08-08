e7cbd3cb62d38ddf2a8558c059cf59f4c5d42237
Ivy Quillosa <ivyquillosa@Ivys-MacBook-Air.local>
2024-08-08 13:14:42 +0800

    update .mdx stories

--- apps/storydocs/.storybook/main.ts
+++ apps/storydocs/.storybook/main.ts
@@ -1,80 +1,29 @@
-// import { dirname, join, resolve } from 'path'
-
-// function getAbsolutePath(value: string): any {
-//   return dirname(require.resolve(join(value, 'package.json')))
-// }
-
-// const config: any = {
-//   // stories: ['../stories/*.stories.tsx', '../stories/**/*.stories.tsx'],
-//   stories: ['../stories/*.mdx', '../stories/*.stories.tsx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-//   addons: [
-//     getAbsolutePath('@storybook/addon-links'),
-//     getAbsolutePath('@storybook/addon-essentials'),
-//     getAbsolutePath('@storybook/addon-docs')
-//   ],
-//   framework: {
-//     name: getAbsolutePath('@storybook/react-vite'),
-//     options: {}
-//   },
-
-//   // core: {
-//   //   builder: '@storybook/builder-vite', // 👈 The builder enabled here.
-//   // },
-
-//   core: {
-//     disableTelemetry: true // 👈 Disables telemetry
-//   },
-
-//   async viteFinal(config, { configType }) {
-//     // customize the Vite config here
-//     return {
-//       ...config,
-//       define: { 'process.env': {} },
-//       resolve: {
-//         extensions: [".mdx", ".mjs", ".js", ".jsx", ".ts", ".tsx"],
-
-//         alias: [
-//           {
-//             find: 'ui',
-//             replacement: resolve(__dirname, '../../../packages/ui/')
-//           }
-//         ]
-//       }
-//     }
-//   },
-
-//   docs: {
-//     autodocs: true
-//   },
-
-//   staticDirs: ['../public']
-// }
-
-// export default config
-import { dirname, join } from 'path'
+import { dirname, join, resolve } from 'path'
 
 function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, 'package.json')))
 }
-
-const config = {
-  stories: ['../stories/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials')
-  ],
+export default {
+  // stories: ['../src/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    stories: ['../stories/*.mdx', '../stories/*.stories.tsx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
-    options: {}
-  },
+        name: getAbsolutePath('@storybook/react-vite'),
+        options: {}
+      },
 
   core: {
-    disableTelemetry: true // 👈 Disables telemetry
+    builder: '@storybook/builder-vite',
   },
-
-  docs: {
-    //👇 See the table below for the list of supported options
-    autodocs: 'tag',
-    defaultName: 'Docs'
-  }
-}
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    const { mergeConfig } = await import('vite');
+ 
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+    });
+  },
+};
